e2a66cdc1e64d8ff63f49702259e7dcb342fb178
Petre Carmen <elenacarmen.petre@gmail.com>
2022-04-11 19:45:47 +0300

    Add templating engine and create poll page
     - added pug templating engine
     - added createPoll get and post endpoints
     - partial createPoll functionality

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
+app.set('view engine', 'pug');
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
+
+app.get('/createPoll',function (req, res){
+    res.sendFile(path.join(__dirname,'./views/createPoll.html'));
+});
+
+app.post('/createPoll', function (req,res){
+   console.log("request body", req.body);
+   res.redirect(303, '/createPoll');
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
