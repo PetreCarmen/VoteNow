2475702b23d2191a0b1e9b09c0502924b9ac22a2
Petre Carmen <elenacarmen.petre@gmail.com>
2022-04-11 21:34:34 +0300

    Add poll view
     - save poll in an in-memory store
     - create poll page that reads poll from memory
     - fix UI for createPoll

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const uuid = require('uuid');
 const port = 3000;
+
+const polls = {}
 
 app.set('view engine', 'pug');
 
@@ -18,8 +21,21 @@ app.get('/createPoll',function (req, res){
 
 app.post('/createPoll', function (req,res){
    console.log("request body", req.body);
-   res.redirect(303, '/createPoll');
+   const pollId = uuid.v4();
+   polls[pollId] = {question: req.body.question, answers: req.body.answers};
+   res.redirect(303, `/poll/${pollId}`);
 });
+
+app.get('/poll/:pollId', (req, res) => {
+    console.log("hello pui");
+    const pollId = req.params["pollId"];
+    if (!polls[pollId]) {
+        console.log("not found");
+        res.status(404).send("Not Found!");
+        return;
+    }
+    res.render('poll', polls[pollId]);
+})
 
 
 app.listen(port, () => {
